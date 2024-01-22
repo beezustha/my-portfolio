46725098757bfbcfce48e25dd7669244a6111cbf
Beezu Shrestha <beezustha@gmail.com>
2024-01-22 23:17:29 +0545

    Added preloader

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,40 @@
+import { useEffect, useState } from "react";
 import { BrowserRouter } from "react-router-dom";
+import Preloader from "./component/Preloader";
 import Footer from "./layout/Footer";
 import Header from "./layout/Header";
 import Experience from "./pages/Experience";
 import Home from "./pages/Home";
 
 function App() {
+	const [loading, setLoading] = useState(true);
+
+	useEffect(() => {
+		// Simulate a delay of 3 seconds
+		const timer = setTimeout(() => {
+			setLoading(false);
+		}, 2000);
+
+		// Clear the timeout on component unmount (cleanup)
+		return () => clearTimeout(timer);
+	}, []); // Empty dependency array means this effect runs once after the initial render
+
 	return (
 		<div className="App">
-			<BrowserRouter>
-				<Header />
-				<Home />
-				{/* <About />
-				<Skills />
-				<Projects /> */}
-				{/* <Experience /> */}
-				{/* <Resume /> */}
-
-				<Footer />
-			</BrowserRouter>
+			{loading ? (
+				<Preloader />
+			) : (
+				<BrowserRouter>
+					<Header />
+					<Home />
+					{/* <About />
+          <Skills />
+          <Projects /> */}
+					{/* <Experience /> */}
+					{/* <Resume /> */}
+					<Footer />
+				</BrowserRouter>
+			)}
 		</div>
 	);
 }
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,39 +1,39 @@
 const Projects = () => {
-  //   const posts = [
-  //     {
-  //       title: "UN Legal Aid",
-  //       img: "https://images.unsplash.com/photo-1556155092-490a1ba16284?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
-  //     },
-  //     {
-  //       title: "Prahar Travels",
-  //       img: "https://images.unsplash.com/photo-1620287341056-49a2f1ab2fdc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
-  //     },
-  //     {
-  //       title: "EPCAN",
-  //       img: "https://images.unsplash.com/photo-1620287341056-49a2f1ab2fdc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
-  //     },
-  //     {
-  //       title: "WRI",
-  //       img: "https://images.unsplash.com/photo-1617529497471-9218633199c0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
-  //     },
-  //   ];
+	//   const posts = [
+	//     {
+	//       title: "UN Legal Aid",
+	//       img: "https://images.unsplash.com/photo-1556155092-490a1ba16284?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+	//     },
+	//     {
+	//       title: "Prahar Travels",
+	//       img: "https://images.unsplash.com/photo-1620287341056-49a2f1ab2fdc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+	//     },
+	//     {
+	//       title: "EPCAN",
+	//       img: "https://images.unsplash.com/photo-1620287341056-49a2f1ab2fdc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+	//     },
+	//     {
+	//       title: "WRI",
+	//       img: "https://images.unsplash.com/photo-1617529497471-9218633199c0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+	//     },
+	//   ];
 
-  return (
-    <section
-      id="projects"
-      className="projects mt-12 mx-auto px-4 max-w-screen-xl md:px-8 bg-green-400"
-    >
-      <div className="text-center">
-        <h2 className="p-3.5 mt-10 text-black-500 text-3xl text-center font-semibold font-acorn">
-          My Projects{" "}
-        </h2>
+	return (
+		<section
+			id="projects"
+			className="projects mt-12 mx-auto px-4 max-w-screen-xl md:px-8 bg-green-400"
+		>
+			<div className="text-center">
+				<h2 className="p-3.5 mt-10 text-black-500 text-3xl text-center font-semibold font-acorn">
+					My Projects{" "}
+				</h2>
 
-        <p className="font-ppneuemontreal mt-3 text-gray-500">
-          These are the some of my projects
-        </p>
-      </div>
-      <div className="mt-12 grid gap-2 sm:grid-cols-2 lg:grid-cols-3">
-        {/* {posts.map((items, key) => (
+				<p className="font-ppneuemontreal mt-3 text-gray-500">
+					These are the some of my projects
+				</p>
+			</div>
+			<div className="mt-12 grid gap-2 sm:grid-cols-2 lg:grid-cols-3">
+				{/* {posts.map((items, key) => (
           <article
             className="max-w-md mx-auto mt-4 shadow-lg border rounded-md duration-300 hover:shadow-sm"
             key={key}
@@ -50,9 +50,9 @@ const Projects = () => {
             </div>
           </article>
         ))} */}
-      </div>
-    </section>
-  );
+			</div>
+		</section>
+	);
 };
 
 export default Projects;
