8d0b010ee54e6a58150198bce34888442d8876dd
Bijaya <bijay@bibhutisolutions.com>
2024-01-24 17:13:00 +0545

    teck stack update

--- src/component/Marquee.jsx
+++ src/component/Marquee.jsx
@@ -1,39 +1,36 @@
-// Marquee.js
-
 import { motion } from "framer-motion";
-import React from "react";
+
+const marqueeVariants = {
+  animate: {
+    x: ["-100%", "100%"],
+    transition: {
+      x: {
+        duration: 10, // Adjust the duration as needed
+        repeat: Infinity,
+        repeatType: "loop",
+        ease: "linear",
+      },
+    },
+  },
+};
 
 const Marquee = () => {
-  const Skills = [
-    { name: "HTML" },
-    { name: "CSS" },
-    { name: "TailwindCSS" },
-    { name: "ReactJS" },
-  ];
   return (
-    <div className="bg-green-700 overflow-hidden">
-      <motion.div
-        className="marquee-container w-full" // Set the width to full width
-        animate={{ x: "100%" }}
-        transition={{
-          repeatType: "loop",
-          ease: "linear",
-          duration: "10",
-        }}
-      >
-        <p>
-          {Skills.map((skill, key) => {
-            return (
-              <>
-                <span className="text-xl "> {skill.key} </span>
-                <span className="text-xl text-red-400"> {skill.name} </span>
-              </>
-            );
-          })}
-        </p>
-
-        {/* Add more items as needed */}
-      </motion.div>
+    <div style={{ overflow: "hidden" }}>
+      <div className="marquee">
+        <motion.div
+          onH
+          className="track"
+          variants={marqueeVariants}
+          initial="initial"
+          animate="animate"
+        >
+          <div className="flex flex-row mr-5">
+            <p>HTML</p>
+            <p>CSS</p>
+          </div>
+        </motion.div>
+      </div>
     </div>
   );
 };
