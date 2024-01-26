ed12ceef8304c40a1269856fa36d6452a99bc91f
Bijaya <bijay@bibhutisolutions.com>
2024-01-26 16:10:13 +0545

    About me updates

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -7,22 +7,6 @@ const Footer = () => {
             Created by Bijaya Shrestha
           </p>
         </div>
-        <div className="max-w-sm rounded overflow-hidden shadow-lg">
-          <img
-            src="path-to-your-image.jpg"
-            alt="Spotify Song Cover"
-            className="w-full h-64 object-cover"
-          />
-          <div className="px-6 py-4">
-            <div className="font-bold text-xl mb-2">Song Title</div>
-            <p className="text-gray-700 text-base">Artist: Artist Name</p>
-          </div>
-          <div className="px-6 py-4">
-            <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
-              Play on Spotify
-            </button>
-          </div>
-        </div>
       </div>
     </>
   );
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,15 +1,19 @@
 import about1 from "../assets/img/about-1.png";
 import about2 from "../assets/img/about-2.png";
+import about3 from "../assets/img/about-3.png";
 
 const About = () => {
   return (
     <>
-      <section id="about" className="bg-teal-600 text-white w-full h-full">
+      <section
+        id="about"
+        className="bg-teal-600 text-white w-full h-full pb-10"
+      >
         {" "}
         <div className="header">
           <h3 className="text-header text-center py-10 font-acorn">About Me</h3>
         </div>
-        <div className="lg:flex flex-row justify-between lg:space-x-4 sm:flex-row sm:space-y-4">
+        <div className="flex flex-col justify-between lg:space-x-4 sm:flex-col sm:space-y-4 lg:flex-row md:flex-row">
           <div className="w-full md:w-1/2 text-[16px] lg:w-[1/2] p-4 lg:pl-16">
             <div className="font-para description text-center sm:text-center md:text-left leading-loose">
               <p>
@@ -30,15 +34,21 @@ const About = () => {
               </p>
             </div>
           </div>
-          <div className="w-full md:w-1/2 lg:w-[1/2] py-4 px-8">
-            <div className="image-section flex flex-row justify-center md:w-full">
+          {/* Card Sections */}
+          <div className="w-full md:w-1/2 lg:w-[1/2] px-10">
+            <div className="grid gap-y-4 gap-x-4 grid-cols-2">
+              <img src={about1} alt="" className="w-full col-span-2" />
+              <img src={about2} alt="" className="w-full h-auto" />
+              <img src={about3} alt="" className="w-full h-auto" />
+            </div>
+            {/* <div className="image-section flex flex-row justify-center md:w-full">
               <img src={about1} alt="" className="w-[1/2] px-6" />
               <img
                 src={about2}
                 alt=""
                 className="hidden md:w-[1/2] lg:w-[1/2] px-6 md:block"
               />
-            </div>
+            </div> */}
           </div>
         </div>
       </section>
